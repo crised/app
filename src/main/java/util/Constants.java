6c5b714fb998cc6062e9d42b858b1a05a2a8684a
crised <crised@gmail.com>
2013-03-06 14:22:28 -0300

    app-19 picture img paths are bad in drip.cl - galleria js

--- src/main/java/util/Constants.java
+++ src/main/java/util/Constants.java
@@ -12,11 +12,11 @@ public final class Constants {
         static final String allCities = "Todas ";
         static final String citiesIn = "Todas en ";
 
-        static final String imgDirectory = "/var/www/html/img";
+        static final String imgDirectory = "/var/www/html/others/img/";
         //drip.cl ->   /var/www/html/img
 
 
-        static final String webPath = "http://23.21.96.77/img/";
+        static final String webPath = "https://drip.cl/others/img/";
         //drip.cl ->   http://23.21.96.77/img/
 
 
